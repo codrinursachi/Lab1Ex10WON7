fada4b1ba205d494cb21c27499a4dc71618f9524
codri <codri@VIRTUAL>
2024-02-15 22:48:46 +0200

    Add project files.

---
+++ Lab1Ex10WON7/Program.cs
@@ -0,0 +1,6 @@
+﻿/*Ex 10
+Scrieti un program care sa va afisa suma cifrelor ununi numar intreg citit de la 
+tastatura.
+ Numarul are exact 2 cifre*/
+const uint numarCifre = 2;
+ShowSum(ComputeSum(ReadNumber(numarCifre)));
---
+++ Lab1Ex10WON7/ProgramHelper.cs
@@ -0,0 +1,27 @@
+﻿partial class Program
+{
+    static string ReadNumber(uint numarCifre)
+    {
+        Console.WriteLine("Scrieti un numar de la tastatura pentru ai afisa suma cifrelor");
+        var numarRaw=Console.ReadLine();
+        if (numarRaw!="" && numarRaw?[0].ToString()=="-")
+            numarRaw=numarRaw.Substring(1);
+        if(numarRaw?.Length!=2 || !int.TryParse(numarRaw, out _))
+        {
+            Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+            return ReadNumber(numarCifre);
+        }
+        return numarRaw;
+    }
+    static int ComputeSum(string numarRaw)
+    {
+        int suma = default;
+        foreach (var cifraRaw in numarRaw)
+            suma += int.Parse(cifraRaw.ToString());
+        return suma;
+    }
+    static void ShowSum(int suma)
+    {
+        Console.WriteLine($"Suma cifrelor este {suma}");
+    }
+}
